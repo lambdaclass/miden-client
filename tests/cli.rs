568e87f0c0e111f710498b9a2ad17ce14b73826b
Jmunoz <juanmunoz890@gmail.com>
2023-11-24 16:58:59 -0300

    initial impl

---
+++ tests/cli.rs
@@ -0,0 +1,50 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use escargot::CargoBuild;
+use predicates::prelude::*; // Used for writing assertions
+
+#[test]
+fn test_cli_create_account_works() -> Result<(), Box<dyn std::error::Error>> {
+    let run = CargoBuild::new()
+        .bin(env!("CARGO_PKG_NAME"))
+        .release()
+        .features("testing")
+        .run()
+        .unwrap()
+        .command()
+        .args([
+            "account",
+            "new",
+            "fungible-faucet",
+            "-t",
+            "TEST",
+            "-d",
+            "10",
+            "-m",
+            "10000",
+        ])
+        .unwrap();
+
+    run.assert().success().stdout(predicate::str::contains(
+        "Succesfully created and stored Account ID:",
+    ));
+
+    Ok(())
+}
+
+#[test]
+fn test_cli_list_accounts() -> Result<(), Box<dyn std::error::Error>> {
+    let run = CargoBuild::new()
+        .bin(env!("CARGO_PKG_NAME"))
+        .release()
+        .features("testing")
+        .run()
+        .unwrap()
+        .command()
+        .args(["account", "list"])
+        .unwrap();
+
+    run.assert()
+        .success()
+        .stdout(predicate::str::contains("| storage root"));
+    Ok(())
+}
