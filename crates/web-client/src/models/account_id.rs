e2c580a961cc3a494ff724d4f0fde6e61c740521
fkrause98 <fkrausear@gmail.com>
2025-07-16 13:48:06 -0300

    docs: specify input for bech32 method & move macro

--- crates/web-client/src/models/account_id.rs
+++ crates/web-client/src/models/account_id.rs
@@ -42,11 +42,11 @@ impl AccountId {
         self.0.to_string()
     }
 
-    #[wasm_bindgen(js_name = "toBech32")]
     /// Will turn the Account ID into its bech32 string representation.
     /// To avoid a potential wrongful encoding, this function will
-    /// expect only ids for either mainnet, testnet or devnet.
+    /// expect only IDs for either mainnet ("mm"), testnet ("mtst") or devnet ("mdev").
     /// To use a custom bech32 prefix, use `Self::to_bech_32_custom`.
+    #[wasm_bindgen(js_name = "toBech32")]
     pub fn to_bech32(&self, network_id: &str) -> Result<String, String> {
         match NetworkId::from_str(network_id) {
             Ok(NetworkId::Custom(_)) => {
@@ -57,9 +57,9 @@ impl AccountId {
         }
     }
 
-    #[wasm_bindgen(js_name = "toBech32Custom")]
     /// Turn this Account ID into its bech32 string representation.
-    /// This method accepts a custom network id.
+    /// This method accepts a custom network ID.
+    #[wasm_bindgen(js_name = "toBech32Custom")]
     pub fn to_bech32_custom(&self, network_id: &str) -> Result<String, String> {
         let network_id = NetworkId::from_str(network_id)
             .map_err(|err| format!("Given network id is not valid: {err}"))?;
